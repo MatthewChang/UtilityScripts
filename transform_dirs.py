be28c9fa3d68b4d10c88080060797d1c8f497ebc
Matthew Chang <mjchang07@gmail.com>
2023-05-27 19:18:15 -0500

    add file number formatting

--- transform_dirs.py
+++ transform_dirs.py
@@ -12,10 +12,15 @@ parser.add_argument('--move',action='store_true')
 args = parser.parse_args()
 num_tokens = 5
 '''
+# match part of file names
 python ~/UtilityScripts/transform_dirs.py  "20bn-something-something-v2-#1.zip" "something/20bn-something-something-v2-#1.zip"
 python ~/UtilityScripts/transform_dirs.py  "fil#1.txt" 'nested#1/fil.txt'
-python ~/UtilityScripts/transform_dirs.py  "##1/fil.txt" 'out/fil.txt'
+
+# glob match multiple directories
 python ~/UtilityScripts/transform_dirs.py  "##1/nested#1/fil.txt" 'out/fil#1.txt'
+
+# disambiguate files with file number
+python ~/UtilityScripts/transform_dirs.py  "##1/nested#1/fil.txt" 'out/fil_%03d.txt'
 '''
 # p1 = '/data01/mc48/latent-diffusion/datasets/claw_data/full_frames/for_scale_2_frames_full/GH010058/raw/#1_mask.png'
 # p2 = '/data01/mc48/latent-diffusion/datasets/claw_data/full_frames/for_scale_2_frames_full/GH010058/raw'
@@ -42,17 +47,18 @@ def to_regex(pat):
 files = glob(to_glob(normpath(args.p1)),recursive=True)
 regex = to_regex(normpath(args.p1))
 outs = []
-for fil in files:
+for fn,fil in enumerate(files):
     match = re.match(regex,fil)
     assert match is not None
     match.groupdict()
     out_path = args.p2
     for k,v in match.groupdict().items():
-        print(k,v)
         if k[0] == 'n':
             out_path = out_path.replace(f'#{k[1:]}',v)
         if k[0] == 'd':
             out_path = out_path.replace(f'##{k[1:]}',v)
+    if '%' in out_path:
+        out_path = out_path % (fn)
     outs.append(out_path)
 
 for fil,out in zip(files,outs):
