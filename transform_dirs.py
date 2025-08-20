e84e8bd44d29ca3f707b8853dde13e6ec578d0ef
matthewchang user <matthewchang@submit-4.fair-a100.hpcaas>
2025-08-20 23:32:59 +0000

    updated some scripts

--- transform_dirs.py
+++ transform_dirs.py
@@ -9,15 +9,17 @@ import argparse
 description = '''
 This utility is used to transform directory paths or filenames by replacing certain patterns. It takes two positional arguments and one optional argument.
 
-Positional arguments:
+Arguments:
 p1: The pattern in the existing file or directory paths. Use '#n' to represent a variable part of the path that will be replaced. Use '##n' to glob match multiple directories.
-p2: The pattern for the new file or directory paths. Use '#n' to represent the part of the path that will be replaced by '#n' in p1.
+output: The pattern for the new file or directory paths. Use '#n' to represent the part of the path that will be replaced by '#n' in p1.
 'n' can be any singe digit integer
-p2 can include formatting strings for integers (e.g. '%03d') which will be replaced by the index of the file or directory in the list of matches.
+output can include formatting strings for integers (e.g. '%03d') which will be replaced by the index of the file or directory in the list of matches.
 
 
 Optional arguments:
---move: If this argument is provided, the files or directories matching p1 will be moved to the new paths specified by p2. If this argument is not provided, the script will try to copy the files instead.
+--move: If this argument is provided, the files or directories matching p1 will be moved to the new paths specified by output. If this argument is not provided, the script will try to copy the files instead.
+
+--delete: Ift this argument is provided, files matching p1 will be deleted. output is not required in this case.
 
 Examples:
 1. To move a file to a nested directory:
@@ -31,11 +33,14 @@ python transform_dirs.py  "##1/nested#1/fil.txt" 'out/fil_%03d.txt'
 '''
 parser = argparse.ArgumentParser(description=description,formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('p1')
-parser.add_argument('p2')
+parser.add_argument('-o', '--output', default=None)
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--move', action='store_true')
 group.add_argument('--delete', action='store_true')
 args = parser.parse_args()
+
+if args.output is None and not args.delete:
+    raise ValueError("You must provide a second argument (output) unless you are deleting files.")
 
 args = parser.parse_args()
 num_tokens = 5
@@ -51,12 +56,12 @@ python ~/UtilityScripts/transform_dirs.py  "##1/nested#1/fil.txt" 'out/fil#1.txt
 python ~/UtilityScripts/transform_dirs.py  "##1/nested#1/fil.txt" 'out/fil_%03d.txt'
 '''
 # p1 = '/data01/mc48/latent-diffusion/datasets/claw_data/full_frames/for_scale_2_frames_full/GH010058/raw/#1_mask.png'
-# p2 = '/data01/mc48/latent-diffusion/datasets/claw_data/full_frames/for_scale_2_frames_full/GH010058/raw'
+# output = '/data01/mc48/latent-diffusion/datasets/claw_data/full_frames/for_scale_2_frames_full/GH010058/raw'
 # p1 = "/Users/mc48/Desktop/20bn-something-something-v2-#1.zip"
-# p2 = "~/Desktop/copy/#1/fil.zip"
+# output = "~/Desktop/copy/#1/fil.zip"
 
 # p1 = "/Users/mc48/Desktop/20bn-something-something-v2-#1.zip"
-# p2 = "/Users/Desktop/something/20bn-something-something-v2-#1.zip"
+# output = "/Users/Desktop/something/20bn-something-something-v2-#1.zip"
 def to_glob(pat):
     for n in range(num_tokens):
         pat = pat.replace(f'##{n}','**')
@@ -74,20 +79,21 @@ def to_regex(pat):
 
 files = glob(to_glob(normpath(args.p1)),recursive=True)
 regex = to_regex(normpath(args.p1))
-outs = []
-for fn,fil in enumerate(files):
-    match = re.match(regex,fil)
-    assert match is not None
-    match.groupdict()
-    out_path = args.p2
-    for k,v in match.groupdict().items():
-        if k[0] == 'n':
-            out_path = out_path.replace(f'#{k[1:]}',v)
-        if k[0] == 'd':
-            out_path = out_path.replace(f'##{k[1:]}',v)
-    if '%' in out_path:
-        out_path = out_path % (fn)
-    outs.append(out_path)
+if not args.delete:
+    outs = []
+    for fn,fil in enumerate(files):
+        match = re.match(regex,fil)
+        assert match is not None
+        match.groupdict()
+        out_path = args.output
+        for k,v in match.groupdict().items():
+            if k[0] == 'n':
+                out_path = out_path.replace(f'#{k[1:]}',v)
+            if k[0] == 'd':
+                out_path = out_path.replace(f'##{k[1:]}',v)
+        if '%' in out_path:
+            out_path = out_path % (fn)
+        outs.append(out_path)
 
 for fil,out in zip(files,outs):
     print(fil,'->',out)
@@ -98,14 +104,15 @@ elif args.move:
 else:
     print("Copy files here? (y/n)")
 if input() == 'y':
-    for fil,out in zip(files,outs):
-        if args.delete:
+    if args.delete:
+        for fil in files:
             print(f"Deleting {fil}")
             if Path(fil).is_dir():
                 shutil.rmtree(fil)
             else:
                 Path(fil).unlink()
-        else:
+    else:
+        for fil,out in zip(files,outs):
             print(fil,'->',out)
             if args.move:
                 Path(out).parent.mkdir(parents=True,exist_ok=True)
